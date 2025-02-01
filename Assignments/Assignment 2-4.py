49cde82250fea2af078c1c786b8e133672bf8614
RedzToxic <daniil1339967@gmail.com>
2025-02-01 18:06:28 -0500

    Pre-Test, the first test of slaary being 1250 and numDepedents being 2.

--- Assignments/Assignment 2-4.py
+++ Assignments/Assignment 2-4.py
@@ -5,7 +5,16 @@ numDependents = float(input())
 # calculate taxes here
 
 #calcualting the state tax
+stateTax = salary * .065
+federalTax = salary * .28
+dependentDeduction = salary * .025 * numDependents
+totalWithholding = stateTax + federalTax + dependentDeduction
+takeHomePay = salary - totalWithholding
 
+# My outputs
+print("State Tax: $" + str(stateTax))
+print("Federal Tax: $" + str(federalTax))
+print("Dependents: $" + str(dependentDeduction))
 # Given output statements
 print("Salary: $" + str(salary))
 print("Take Home Pay: $" + str(takeHomePay))
