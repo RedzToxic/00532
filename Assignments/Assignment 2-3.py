5bd435897251dea75ffee6f7d6e5458c139e4c6a
RedzToxic <daniil1339967@gmail.com>
2025-02-01 17:05:44 -0500

    Completed assignment 2-3

--- Assignments/Assignment 2-3.py
+++ Assignments/Assignment 2-3.py
@@ -1,11 +1,18 @@
 # Furniture.py - This program calculates profits and sales prices for a furniture company.
 
-# All declarations
+# All initial declarations of the name, and how much a TV stand costs in either retail or whole sale pricing.
 itemName = "TV Stand"
 retailPrice = 325.00
 wholesalePrice = 200.00
 
 # Write your assignment statements here for profit, salePrice, and saleProfit
+
+# Calculating the profit by subtractin the retailPrice from wholesalePrice
+profit = retailPrice - wholesalePrice
+# Calculating the sale price as being deducted for 25% from retail price
+salePrice = .75 * retailPrice
+# Calculating the profit by using the sale profit with sale price minus the wholesale price
+saleProfit = salePrice - wholesalePrice
 
 print("Item Name: " + itemName)
 print("Retail Price: $" + str(retailPrice))
