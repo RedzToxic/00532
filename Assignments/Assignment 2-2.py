83a2dd4610f9e5232f561136a71853894ee188d2
RedzToxic <daniil1339967@gmail.com>
2025-02-01 16:06:41 -0500

    Assignent 2-2 added

---
+++ Assignments/Assignment 2-2.py
@@ -0,0 +1,14 @@
+# This program calculates your age in the year 2050.
+# Input:  None
+# Output: Your current age followed by your age in 2050
+
+# Create your variables here
+# Assigns my current age
+myCurrentAge = 18
+# Assigns the current year
+currentYear = 2025
+
+# Calculates how old I will be in 2050, by grabbing current age and adding to 2050 - the current year
+myNewAge = myCurrentAge + (2050 - currentYear)
+print("My Current Age is " + str(myCurrentAge))
+print("I will be " + str(myNewAge) + " in 2050.")
