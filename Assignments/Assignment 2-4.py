69fdf10f49539c9e4e4606ba832520ba96cc780c
RedzToxic <daniil1339967@gmail.com>
2025-02-01 17:59:18 -0500

    Adding Assigment 2-4

---
+++ Assignments/Assignment 2-4.py
@@ -0,0 +1,11 @@
+# input statements
+salary = float(input())
+numDependents = float(input())
+
+# calculate taxes here
+
+#calcualting the state tax
+
+# Given output statements
+print("Salary: $" + str(salary))
+print("Take Home Pay: $" + str(takeHomePay))
