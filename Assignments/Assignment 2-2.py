ded3c8581021486cd090ef0722c0844a8e35984a
RedzToxic <daniil1339967@gmail.com>
2025-02-01 16:28:15 -0500

    Added addtional comments/ pesudocode to make sure it's as clear as possible

--- Assignments/Assignment 2-2.py
+++ Assignments/Assignment 2-2.py
@@ -3,12 +3,14 @@
 # Output: Your current age followed by your age in 2050
 
 # Create your variables here
-# Assigns my current age
+# Assigns myCurrentAge to a static number, 18
 myCurrentAge = 18
-# Assigns the current year
+# Assigns currentYear to static number, 2025
 currentYear = 2025
 
-# Calculates how old I will be in 2050, by grabbing current age and adding to 2050 - the current year
+# Calculates how old I will be in 2050, by grabbing myCurrentAge and adding to (2050 - the currentYear)
 myNewAge = myCurrentAge + (2050 - currentYear)
+# Printing/ out puting string and converting the myCurrentAge to string
 print("My Current Age is " + str(myCurrentAge))
+# Printing/ out puting a string and converting the myNewAge to string
 print("I will be " + str(myNewAge) + " in 2050.")
