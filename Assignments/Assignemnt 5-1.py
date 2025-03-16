716cd489c11259dbaee7cfa33e5393b415b08dd9
RedzToxic <daniil1339967@gmail.com>
2025-03-15 20:00:28 -0400

    Finished Assignment 5-1

--- Assignments/Assignemnt 5-1.py
+++ Assignments/Assignemnt 5-1.py
@@ -1,2 +1,79 @@
-#make loops
+# make loops
 
+# a)
+'''
+a = 1
+b = 2
+c = 5
+
+while (a < c):
+    a = a + 1
+    b = b + c
+
+print(a, b, c)
+'''
+
+# b)
+'''
+d = 4
+e = 6
+f = 7
+
+while (d > f):
+    d = d + 1
+    e = e - 1
+
+print(d, e, f)
+'''
+
+# c)
+'''
+g = 4
+h = 6
+
+while (g < h):
+    g = g + 1
+
+print(g, h)
+'''
+
+# d)
+'''
+j = 2
+k = 5
+n = 9
+
+
+while (j < k):
+    m = 6
+    while (m < n):
+        print("Goodbye")
+        m = m + 1
+    j = j + 1
+'''
+
+# e)
+'''
+j = 2
+k = 5
+m = 6
+n = 9
+while (j < k):
+    while (m < n):
+        print("Hello")
+        m = m + 1
+    j = j + 1
+'''
+
+# f)
+
+p = 2
+q = 4
+while (p < q):
+    print("Adios")
+    r = 1
+    while (r < q):
+        print("Adios")
+        r = r + 1
+    p = p + 1  
+ 
